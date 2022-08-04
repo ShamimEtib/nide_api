b1dae7af4209dbb295eb99492f0576002cffbb67
ShamimEtib <etib.shamim123@gmail.com>
2022-08-04 14:05:15 +0600

    port

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 8000
 const server = http.createServer(app)
 
 server.listen(port, () => {
-  console.log('App is listening on port 8000!')
+  console.log(`App is listening on port ${port}!`)
 });
